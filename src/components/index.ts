2768607e372841630d0dd6b880208ee784da2259
Charles <cjgantt@gmail.com>
2024-05-15 05:06:00 -0400

    changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,6 @@
 "use client";
 
 export * from "./navbar";
-export * from "./footer";
 export * from "./layout";
-export * from "./footer";
 export * from "./blog-post-card";
 export * from "./fixed-plugin";
