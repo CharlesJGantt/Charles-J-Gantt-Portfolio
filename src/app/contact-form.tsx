d3ee16b2a0fbfc54da35479f703f6770922d027a
Charles <cjgantt@gmail.com>
2024-05-19 15:31:48 -0400

    changes

--- src/app/contact-form.tsx
+++ src/app/contact-form.tsx
@@ -52,7 +52,7 @@ export function ContactForm() {
        </p> 
        
       </Typography>
-    <div className="container mx-auto mb-5 md:mb-20 text-center">
+    <div className="container mx-auto mb-5 md:mb-1 text-center">
       
       <Typography variant="h1" color="blue-gray" className="mb-4" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
 
